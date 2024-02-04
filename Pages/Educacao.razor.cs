df665ac1b3c3e040cf42aa3eff5cbd031b94a724
Túlio Nunes Polido Lopes <tulionp.lopes@gmail.com>
2024-02-04 18:55:47 -0300

    Adicionada página de educação com cards de Graduação e Mestrado

--- Pages/Educacao.razor.cs
+++ Pages/Educacao.razor.cs
@@ -1,5 +1,9 @@
+using Microsoft.AspNetCore.Components;
+
 namespace Portfolio.Pages {
     public partial class Educacao {
-
+        protected override void OnInitialized()
+        {
+        }
     }
 }
--- obj/Debug/net8.0/Portfolio.AssemblyInfo.cs
+++ obj/Debug/net8.0/Portfolio.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a998c0f426a1b94ddf75c0eb74874c540b062ad9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d58442dfafa96e9c89028dbb8ed9121e94fd9734")]
 [assembly: System.Reflection.AssemblyProductAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
